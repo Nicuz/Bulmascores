caf47981c2579c85ca78d5084c399db9fb1d040b
isibsi <narsada89@gmail.com>
2019-08-30 15:45:54 +0900

    カスタムポストを順番に表示する関数を手直し

--- inc/custom_post_type.php
+++ inc/custom_post_type.php
@@ -83,26 +83,71 @@ if ( ! function_exists( 'bulma_get_archive_custom_posts' ) ) {
         $tax_posts = get_posts( $tax_get_array );
         // ポストが存在するならば
         if($tax_posts):
-          echo '<section class="column is-12-mobile is-6-tablet is-4-desktop front-section">';
-          echo  '<div class="front-section__columns columns is-mobile">';
+          echo  '<section class="front-section">';
         //  echo  '<div class="column is-2">';
         //  echo    '<span class="front-icon">'.shard_fontawesome_random($taxonomy->term_id).'</span>'; // アイコンをtermi_idを元にしてランダムに生成する
         //  echo  '</div>';
-          echo  '<div class="front-section__content column">';
-          echo    '<h2 class="front-heading" id="' . esc_html($taxonomy->slug) . '">';
-          echo      '<a href="'. $url_taxonomy .'">'. esc_html($taxonomy->name) .'</a>';
+          echo    '<h2 class="title is-2 front-section__heading" id="' . esc_html($taxonomy->slug) . '">';
+          //echo      '<a href="'. $url_taxonomy .'">'. esc_html($taxonomy->name) .'</a>';
+          echo    esc_html($taxonomy->name);
           echo    '</h2>';
-          echo    '<ul class="front-list">';
+          echo    '<div class="front-section__content">';
             foreach($tax_posts as $tax_post):
-               echo '<li class="front-listItem"><a href="'. get_permalink($tax_post->ID).'">'. get_the_title($tax_post->ID).'</a></li>';
+               echo '<article class="front-section__article">';
+               // echo '<h3 class="front-listItem"><a href="'. get_permalink($tax_post->ID).'">'. get_the_title($tax_post->ID).'</a></h3>';
+               $custom_post = get_post($tax_post->ID);
+               echo '<h3 class="title is-3 front-section__article__title">'. get_the_title($tax_post->ID).'</h3>';
+               echo '<div class="columns">';
+               echo   '<div class="column">'.$custom_post->post_content.'</div>';
+               echo   '<div class="column">' .get_the_post_thumbnail( $tax_post->ID , 'thumbnail' ).'</div>';
+               echo '</div>';
+               echo '<a class="button" href="'.get_the_permalink($tax_post->ID).'">記事の詳細</a>';
+               echo '</article>';
             endforeach;
             wp_reset_postdata();
-          echo     '</ul>';
-          echo    '</div>';// #front-section__content
-          echo   '</div>';
-          echo '</section>';
+          echo    '</div>';
+          echo  '</section>';
         endif;
       } // end foreach
     } // end if
   } // end function
 } // end exist
+
+
+if ( ! function_exists( 'get_bulma_archive_custom_posts' ) )
+{
+  function get_bulma_archive_custom_posts( $taxonomy_name = 'front_about',$post_type= 'front') 
+  {
+    $args = array(
+        'orderby' => 'name',
+        'hierarchical' => false
+    );
+    $taxonomys = get_terms( $taxonomy_name, $args);
+
+    if(!is_wp_error($taxonomys) && count($taxonomys))
+    {
+      foreach($taxonomys as $taxonomy)
+      {
+        $url_taxonomy = get_term_link($taxonomy->slug, $taxonomy_name);
+        $tax_get_array = array(
+            'post_type' => $post_type, //表示したいカスタム投稿
+            'posts_per_page' => 5,//表示件数
+            // https://blog.nakachon.com/2014/10/27/dont-use-name-field-tax-query-in-japanese/
+            // termsにはidを, fieldにはterm_idを入れるべき
+            'tax_query' => array(
+              array(
+                      'taxonomy' => $taxonomy_name,
+                      'terms'     => array($taxonomy -> term_id),
+                      'field'    => 'term_id',
+                      'operator' => 'IN',
+                      'include_children' => true
+              )
+            )
+        );
+        $tax_posts = get_posts( $tax_get_array );
+      }
+      return $tax_posts;
+    }
+    return 0;
+  }
+}
