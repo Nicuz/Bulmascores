a96d70e6a2226e02ea5768da657ac0efea881766
yugioh-hack <aliusneos@gmail.com>
2019-09-02 14:11:27 +0900

    カスタムフィールドを分離

--- functions.php
+++ functions.php
@@ -184,5 +184,8 @@ require get_template_directory() . '/inc/breadcrumb.php';
 // カスタム投稿タイプ 
 require get_template_directory() . '/inc/custom_post_type.php';
 
+// カスタムフィールド
+require get_template_directory() . '/inc/custom_fields.php';
+
 // 一般投稿の表示に関する制御
 require get_template_directory() . '/inc/get-post.php';
