540e154785d53793af18ba67c9423f776e97428a
YugiohHack <aliusneos@gmail.com>
2019-09-08 00:18:21 +0900

    トップページの参加ボタン周辺を改良して、背景画像を少し活かす配置にした

--- template-parts/contents/content-frontpage-hero.php
+++ template-parts/contents/content-frontpage-hero.php
@@ -1,9 +1,11 @@
 <section style="background: linear-gradient(rgba(0,0,0, 0.4),rgba(0,0,0, 0.4)), url(<?php echo get_template_directory_uri(). '/assets/img/top-hero.jpg' ; ?>); background-size: cover; background-position: center center;" class="hero is-primary is-large">
-    <div class="hero-body">
-        <div class="container has-text-centered">
-            <h1 class="title is-1"><?php bloginfo('name'); ?></h1>
-            <h2 class="subtitle is-3"><?php bloginfo('description'); ?></h2>
-            <a class="button is-large is-rounded is-info" href="//ingress.com/ja/events/">参加</a>
+    <div class="hero is-medium">
+        <div class="hero-body">
+            <div class="hero-mdcenter container has-text-centered">
+                <h1 class="title is-1"><?php bloginfo('name'); ?></h1>
+                <h2 class="subtitle is-3"><?php bloginfo('description'); ?></h2>
+                <a class="button is-large is-rounded is-info" href="//ingress.com/ja/events/">参加</a>
+            </div>
         </div>
     </div>
 </section>
