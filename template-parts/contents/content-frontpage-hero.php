fcfe09b54fd3ccc78069f2ced5719af96343bb24
YugiohHack <aliusneos@gmail.com>
2019-09-09 01:19:02 +0900

    mixinを入れなおした

--- template-parts/contents/content-frontpage-hero.php
+++ template-parts/contents/content-frontpage-hero.php
@@ -1,6 +1,6 @@
 <section style="background: linear-gradient(rgba(0,0,0, 0.4),rgba(0,0,0, 0.4)), url(<?php echo get_template_directory_uri(). '/assets/img/top-hero.jpg' ; ?>); background-size: cover; background-position: center center;" class="hero is-primary is-large">
     <div class="hero is-medium">
-        <div class="hero-body">
+        <div class="hero-body--2">
             <div class="hero-mdcenter container has-text-centered">
                 <h1 class="title is-1"><?php bloginfo('name'); ?></h1>
                 <h2 class="subtitle is-3"><?php bloginfo('description'); ?></h2>
